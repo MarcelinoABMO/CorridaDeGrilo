075d4c5f531975c5a17c4603ccc1b8793d2611b9
GustavoEli <gustavoeliseu99@gmail.com>
2021-07-27 15:59:13 -0300

    adicionando variaveis a classe grilo

--- CorridaDeGrilo/src/corridadegrilo/Grilo.java
+++ CorridaDeGrilo/src/corridadegrilo/Grilo.java
@@ -1,8 +1,14 @@
 package corridadegrilo;
 
+import java.util.Random;
+
 public class Grilo{
     
+    private Random  random = new Random();
+    private int posicao;
+    
     void Pulo(){
+        posicao += random.nextInt(10);
         
     }
 }
