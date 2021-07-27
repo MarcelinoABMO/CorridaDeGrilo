ccd27b327428e20e361c0b541d8b782962dc3c1d
GustavoEli <gustavoeliseu99@gmail.com>
2021-07-27 16:12:28 -0300

    adicionando variaveis de pulo, alem do random e pondo comando de console

--- CorridaDeGrilo/src/corridadegrilo/Grilo.java
+++ CorridaDeGrilo/src/corridadegrilo/Grilo.java
@@ -6,9 +6,17 @@ public class Grilo{
     
     private Random  random = new Random();
     private int posicao;
+    private int indice;
+    
+    public Grilo(int index){
+        this.indice = index;
+    
+    }
     
     void Pulo(){
-        posicao += random.nextInt(10);
+        int pegaPulo = random.nextInt(10);
+        posicao += pegaPulo;
         
+        System.out.println("O grilo " + indice + " pulou " + pegaPulo + " e ja percorreu " + posicao + "cm");
     }
 }
